fe1cdf67dec2d4127a144220ddfb15ef43237623
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-20 19:31:30 -0700

    s

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c, m;
+
+	for (c = 'a', c <= 'z', c++);
+	{
+		putchar(c);
+	}
+	for (m = 'A', m <= 'Z', m++);
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
