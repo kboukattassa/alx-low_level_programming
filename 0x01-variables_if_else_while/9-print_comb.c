abd6b5b541381b56f4f26bc07dacc86800dd0882
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-22 14:07:23 -0700

    s

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,12 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + 48);
-		putchar(',');
-	}
-
-	for (i = 0; i < 9; i++)
-	{
-		putchar(' ');
+		if (i < 9)
+		{
+			putchar(i + 48);
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
