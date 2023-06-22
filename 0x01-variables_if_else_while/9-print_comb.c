b7b24d9fed3ce68aebb9c9b8c6cbda9c16c8500a
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-22 11:19:11 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers seperated by a ,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i + 48);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
