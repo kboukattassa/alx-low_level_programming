d130db95c2eff21aed72da4c41081c7eeb5e4a5a
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-23 18:07:59 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alpha x10
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
