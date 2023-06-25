84dbe32624d6dc79cfd28a5891b388460475a75b
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-25 15:35:59 -0700

    s

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,14 @@ void jack_bauer(void)
 
 	for (hour = 0; hour < 24; hour++)
 	{
-		for (minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60)
 		{
-			_putchar("%02d:%02d\n", hour, minute);
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
