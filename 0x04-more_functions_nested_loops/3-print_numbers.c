4b8b76587cadec34584c4b484ba60663b9f8d1f4
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-25 20:15:35 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+	return (0);
+}
