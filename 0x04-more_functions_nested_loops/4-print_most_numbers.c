9c31875742b40fbbcc1fdf18862db505bbf182db
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-06-26 09:15:19 -0700

    s

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints from 0 to 9 except 2 & 4
+ *
+ * Return: numbers
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if (n != 60 || n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
