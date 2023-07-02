d96bd6b7383b4a71e972bba9e6099bed19831e4c
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-07-01 17:36:59 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - Prints half of a string
+ *
+ * Return: Result
+ */
+void puts_half(char *str)
+{
+	int n;
+
+	if (_strlen(str) % 2 == 1)
+	{
+		n = (_strlen(str) - 1 ) / 2;
+	}
+	else
+	{
+		n = (_strlen(str) ) / 2;
+	}
+	for (; n < _strlen(str); n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
