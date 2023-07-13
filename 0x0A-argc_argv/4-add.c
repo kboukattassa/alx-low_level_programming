ea4dd24712fd4e2b4c57547ec2f660b2ae98798f
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-07-12 18:36:06 -0700

    s

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - multiplies teo numbers
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc == 3)
+        {
+                printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+                return (0);
+        }
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
