8d0dc3cee43da4105e54543a529c9a2a3bb95585
Mkhalil <17323096+kboukattassa@users.noreply.github.com>
2023-08-01 15:49:55 -0700

    s

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - function that counts the number of elements
+ * @h: pointer to the head
+ * Return: returns the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
