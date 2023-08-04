b9a055195d1f536194929aae26ad88becc7d9861
kboukattassa <17323096+kboukattassa@users.noreply.github.com>
2023-08-04 21:51:15 +0100

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef VARIADIC_FUNC
-#define VARIADIC_FUNC
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-#endif
