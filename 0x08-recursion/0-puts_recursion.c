129a0a078166a30c3c8b72119945554c47042520
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-08-30 22:29:58 +0000

    created a recursive function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion: prints string using recur
+ * @s: charachter pointer
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
