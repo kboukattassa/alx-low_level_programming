633268724093022680b35a52b414a5bcfabab11e
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-08-31 00:30:53 +0000

    created a func that calculates the value of x to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value
+ * of base x raised to the power of y
+ * @x: para
+ * @y: para
+ * Return: calculate the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
