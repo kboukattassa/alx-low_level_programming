574e1e5d7fc6c435aba082a3d2159ca5741a3841
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-16 15:39:02 +0000

    created a func that iterates through all the elements of the array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_iterator - prints each array elem
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to a fun that returns void
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for(i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
