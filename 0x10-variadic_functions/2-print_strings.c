ce633b2d8ed46629bb8a83a386be6dc0756dd08a
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-17 19:45:51 +0000

    fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,10 +27,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 		}
-		if (index != (n - 1) && separtor != NULL)
+		if (index != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
+	}
 		printf("\n");
 		va_end(strings);
 }
