f411166a7b6f8756ca9b730adb37d4b53edad915
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-23 15:45:26 +0000

    count = 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node_count;
+	size_t node_count = 0;
 
 	while (h != NULL)
 	{
@@ -19,10 +19,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("%u %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
-		node_count++;
 		h = h->next;
+		node_count++;
 	}
 	return (node_count);
 }
