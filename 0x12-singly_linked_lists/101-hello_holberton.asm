86d41b5f118e7e4532bf5c8372b3b34ecb8455fb
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-24 01:48:31 +0000

    s

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,19 @@
 section .data
-     msg db "Hello, Holberton", 10
+    hello db "Hello, Holberton",10  ; The message with a newline character
+    hello_len equ $ - hello          ; Calculate the message length
 
- section .text
-     global main
-    extern printf
-main:
-    push rbp
-    mov rbp, rsp
-    lea rdi, [msg]
-     xor eax, eax
-     call printf
+section .text
+    global _start
 
-     leave
-     ret
+_start:
+    ; Prepare arguments for the write system call
+    mov rax, 1                  ; syscall number for write (1)
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    mov rsi, hello              ; pointer to the message
+    mov rdx, hello_len          ; message length
+    syscall                     ; invoke the syscall
+
+    ; Exit the program
+    mov rax, 60                 ; syscall number for exit (60)
+    xor rdi, rdi                ; exit status (0)
+    syscall                     ; invoke the syscall
