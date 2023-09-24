794af29f414242db17b9f51415973777e58ff78c
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-24 01:47:12 +0000

    test

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,15 @@
 section .data
-    format db "Hello, Holberton",10,0  ; The format string with a newline character and null-terminated
+     msg db "Hello, Holberton", 10
 
-section .text
-    global main
+ section .text
+     global main
     extern printf
-
 main:
     push rbp
-    mov rdi, format
-    call printf
-    pop rbp
-    ret
+    mov rbp, rsp
+    lea rdi, [msg]
+     xor eax, eax
+     call printf
+
+     leave
+     ret
