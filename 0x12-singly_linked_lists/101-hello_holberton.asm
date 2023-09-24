90133e2db15054270b2c0e945c9299797dfede8d
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-24 01:50:29 +0000

    check 4 fail

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,13 @@
 section .data
-    hello db "Hello, Holberton",10  ; The message with a newline character
-    hello_len equ $ - hello          ; Calculate the message length
+    format db "Hello, Holberton",10,0  ; The format string with a newline character and null-terminated
 
 section .text
-    global _start
+    global main
+    extern printf
 
-_start:
-    ; Prepare arguments for the write system call
-    mov rax, 1                  ; syscall number for write (1)
-    mov rdi, 1                  ; file descriptor 1 (stdout)
-    mov rsi, hello              ; pointer to the message
-    mov rdx, hello_len          ; message length
-    syscall                     ; invoke the syscall
-
-    ; Exit the program
-    mov rax, 60                 ; syscall number for exit (60)
-    xor rdi, rdi                ; exit status (0)
-    syscall                     ; invoke the syscall
+main:
+    push rbp
+    mov rdi, format
+    call printf
+    pop rbp
+    ret
