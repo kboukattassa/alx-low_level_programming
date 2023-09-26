f9d2499240f48c94527c2005708235bec535f8e4
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-26 15:08:02 +0000

    frees node at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * delete_nodeint_at_index - deletes node at index
+ * @head: pointer to pointer to head
+ * @temp: tempo node
+ * @new_node: new node
+ * 
+ * Return: return
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *new_node, *temp;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	new_node = *head;
+	while (new_node != NULL)
+	{
+		if (i == index - 1)
+		{
+			if (new_node->next == NULL)
+				return (-1);
+			temp = new_node->next;
+			new_node->next = temp->next;
+			free(temp);
+			return(1);
+		}
+		new_node = new_node->next;
+		i++;
+	}
+	return (-1);
+}
