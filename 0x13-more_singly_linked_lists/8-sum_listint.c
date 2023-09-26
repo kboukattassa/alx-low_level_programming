ba2e11946f28f31aa64fec2e5ef050458e11f06d
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-26 01:42:23 +0000

    func that sums the data in the lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_listint - sums up the data in the list
+ * @head: pointer to the head of the list
+ * @sum: int
+ * @temp: tempo pointer
+ *
+ * Return: the sum of the data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
