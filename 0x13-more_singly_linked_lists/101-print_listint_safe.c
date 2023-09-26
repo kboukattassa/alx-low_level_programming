164f25a83d76c234dddb1b5528f1302932c27b02
MKhalil <17323096+kboukattassa@users.noreply.github.com>
2023-09-26 16:43:26 +0000

    s

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,31 @@
-#include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * print_listint_safe - prints list safe from loops
- * @head: pointer to head
- * @slow: pointer
- * @fast: moves 2x as slow pointer
- * @count: counter
- *
- * Return: number of elements
- */
-size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
-	size_t count = 0;
+ 	size_t count = 0;
+ 	size_t i;
+ 	const listint_t *ptr, *temp;
 
-	if (head == NULL)
-		return 98;
-	while(slow && fast && fast->next)
-	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
-		count++;
-
-		if(slow == fast)
-		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			return (count);
-		}
-	}
-	return (count);
-}
+ 	if (head == NULL)
+ 		exit(98);
+ 	ptr = head;
+ 	while (ptr != NULL)
+ 	{
+ 		printf("[%p] %d\n", (void *)ptr, ptr->n);
+ 		ptr = ptr->next;
+ 		count++;
+ 		i = 0;
+ 		temp = head;
+ 		while (i < count)
+ 		{
+ 			if (temp == ptr)
+ 			{
+ 				printf("-> [%p] %d\n", (void *)ptr, ptr->n);
+ 				return (count);
+ 			}
+ 			else
+ 			{
+ 				temp = temp->next;
+ 				i++;
+ 			}
+ 		}
+ 	}
+ 	return (count);
+ }
